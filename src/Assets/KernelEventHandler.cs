ddde8ce0e8a56aec658a886898e76886cd7d5c1b
Andrii Semchenko <a7chenko@gmail.com>
2019-12-20 23:01:55 +0200

    Impelemented opening craft menu

---
+++ src/Assets/KernelEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KernelEventHandler : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked on kernel!");
+        var menuManager = GameObject.Find("MenuManager").GetComponent<MenuManager>();
+        menuManager.OpenCraftMenu();
+    }
+}
