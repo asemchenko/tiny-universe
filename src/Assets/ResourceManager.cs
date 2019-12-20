d8891417b7b80aae6cc85e76b1aec142018c9fb6
Andrii Semchenko <a7chenko@gmail.com>
2019-12-20 15:22:56 +0200

    Added resources manager

--- src/Assets/ResourceManager.cs
+++ src/Assets/ResourceManager.cs
@@ -1,26 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
+/**
+ * This script is used to manage resource. All resources should be updated only via this script, not directly!
+ */
 public class ResourceManager : MonoBehaviour
 {
-    public long timeResourceAmount = 500;
+    // real resource values
+    private long _energyResourceAmount;
+    private long _spaceResourceAmount;
+    private long _substanceResourceAmount;
+    private long _timeResourcesAmount;
+    // resource view pointers
+    public Text energyResourceText;
+    public Text spaceResourceText;
+    public Text substanceResourceText;
+    public Text timeResourceText;
 
-    public long spaceResourceAmount = 500;
+    // resource properties
+    public long TimeResourceAmount
+    {
+        get => _timeResourcesAmount;
+        set
+        {
+            timeResourceText.text = value.ToString();
+            _timeResourcesAmount = value;
+        }
+    }
 
-    public long substanceResourceAmount = 500;
+    public long SpaceResourceAmount
+    {
+        get => _spaceResourceAmount;
+        set
+        {
+            spaceResourceText.text = value.ToString();
+            _spaceResourceAmount = value;
+        }
+    }
 
-    public long energyResourceAmount = 500;
+    public long SubstanceResourceAmount
+    {
+        get => _spaceResourceAmount;
+        set
+        {
+            substanceResourceText.text = value.ToString();
+            _substanceResourceAmount = value;
+        }
+    }
+
+    public long EnergyResourceAmount
+    {
+        get => _energyResourceAmount;
+        set
+        {
+            energyResourceText.text = value.ToString();
+            _energyResourceAmount = value;
+        }
+    }
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        Debug.Log("Initializing resources");
+        TimeResourceAmount = 300;
+        SpaceResourceAmount = 200;
+        SubstanceResourceAmount = 400;
+        EnergyResourceAmount = 100;
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
     }
-    
-    
 }
