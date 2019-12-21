095bb2a26a2cecd945a83d9b5510b58d2b428b7b
KorolovaLuda <luda_k1@ukr.net>
2019-12-21 07:18:37 +0200

    Add files via upload

---
+++ src/Assets/CreateObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreateObject : MonoBehaviour
+{
+    public GameObject prefab;
+    public Scene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject result = Instantiate(prefab, new Vector3(3, 0, 0), Quaternion.identity);
+        result.GetComponent<MeshRenderer>().sortingOrder = 2;
+        SceneManager.MoveGameObjectToScene(result, scene);
+    }
+
+
+}
