036dab5c78947e557c3cd1e15526e9dc7e503268
Andrii Semchenko <a7chenko@gmail.com>
2019-12-23 15:31:13 +0200

    Added tooltip for Galaxy

---
+++ src/Assets/ToolTipScripts/GalaxyTooltipScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using model;
+using ToolTipScripts;
+using UnityEngine;
+
+public class GalaxyTooltipScript : AbstractTooltipScript
+{
+    private Galaxy stub = new Galaxy();
+    protected override string getTooltipTitle()
+    {
+        return stub.GetResourceName();
+    }
+
+    protected override string getTooltipDescription()
+    {
+        return stub.GetResourceDescription();
+    }
+}
